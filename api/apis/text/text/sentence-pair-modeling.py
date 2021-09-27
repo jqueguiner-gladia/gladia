6399361c91a0937f9f48921431eaa72de5a32880
jqueguiner <jean-louis.queguiner@gadz.org>
2021-09-27 21:22:20 +0000

    add sentence pair modeling

---
+++ api/apis/text/text/sentence-pair-modeling.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter
+from ai_api_utils.submodules import TaskRouter
+
+router = APIRouter()
+
+input = [
+    {
+        "type": "text",
+        "name": "sentence",
+        "default": "Once, a group of frogs was roaming around the forest in search of water.",
+        "placeholder": "Once, a group of frogs was roaming around the forest in search of water.",
+        "tooltip": "Insert the sentence to perform the Pairwise Sentence Scoring Tasks",
+    },
+]
+
+output = [
+    {
+        "type": str,
+        "name": "analyzed_sentence",
+    }
+]
+
+
+TaskRouter(router=router, input=input, output=output, default_model="UKPLab-all-MiniLM-L6-v2")
