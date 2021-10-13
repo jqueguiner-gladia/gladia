9fadb2c3eeb4088966fa2df2796202570a4ca7d1
jqueguiner <jean-louis.queguiner@gadz.org>
2021-10-13 21:23:50 +0000

    [Docker][Entrypoint] add possibility to espace entrypoint in helper

--- api/build_and_prod.sh
+++ api/build_and_prod.sh
@@ -1,1 +1,1 @@
-docker build -t unifai-apis -f gpu.Dockerfile.prod . && docker run -d -p 8080:80 unifai-apis
+docker build -t unifai-apis -f gpu.Dockerfile.prod . && docker run -d -p 8080:80 unifai-apis $1
