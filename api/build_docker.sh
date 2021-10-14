2087faffe7af8f28f94a16ea0fd50994eda619b9
jqueguiner <jean-louis.queguiner@gadz.org>
2021-10-14 07:56:53 +0000

    add docker build andpush script

--- api/build_docker.sh
+++
@@ -1,3 +0,0 @@
-docker build -t databuzzword/ai-api-marketplace $1 -f gpu.Dockerfile .
-docker tag databuzzword/ai-api-marketplace databuzzword/ai-api-marketplace
-docker push databuzzword/ai-api-marketplace
