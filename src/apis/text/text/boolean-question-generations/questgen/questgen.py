334d0139bb1c4a149783e8a972e0076f9ef74638
Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>
2021-10-29 23:27:54 +0200

    Update questgen.py

--- src/apis/text/text/boolean-question-generations/questgen/questgen.py
+++ src/apis/text/text/boolean-question-generations/questgen/questgen.py
@@ -1,11 +1,13 @@
---2021-10-29 12:38:28--  https://raw.githubusercontent.com/theunifai/unifai-apis-core/main/src/apis/text/text/boolean-question-generations/questgen/questgen.py
-Resolving raw.githubusercontent.com (raw.githubusercontent.com)... 185.199.109.133, 185.199.110.133, 185.199.111.133, ...
-Connecting to raw.githubusercontent.com (raw.githubusercontent.com)|185.199.109.133|:443... connected.
-HTTP request sent, awaiting response... 200 OK
-Length: 268 [text/plain]
-Saving to: ‘questgen.py’
+from pprint import pprint
+import nltk
+nltk.download('stopwords')
+from Questgen import main
+import json
 
-     0K                                                       100% 11.5M=0s
-
-2021-10-29 12:38:28 (11.5 MB/s) - ‘questgen.py’ saved [268/268]
-
+def predict(text):
+    qe= main.BoolQGen()
+    payload = {
+        "input_text": text
+        }
+    output = qe.predict_boolq(payload)
+    return json.dumps(output)
