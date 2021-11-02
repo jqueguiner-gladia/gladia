1eeb438c6d79d92401178b5024a657cdbce7f52a
jqueguiner <jean-louis.queguiner@gadz.org>
2021-11-02 11:56:58 +0000

    move sdk to external repo

--- sdk/clients/clojure/project.clj
+++
@@ -1,7 +0,0 @@
-(defproject fast-api "0.1.0"
-  :description "Client library of fast-api"
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [metosin/spec-tools "0.7.0"]
-                 [clj-http "3.8.0"]
-                 [orchestra "2017.11.12-1"]
-                 [cheshire "5.8.0"]])
