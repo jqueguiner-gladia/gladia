e18e7dfdb6667b6bcac9f1449b94a6c816da1be0
Valentin De Matos <valentin.de-matos@epitech.eu>
2022-03-14 14:45:48 +0100

    delete: unused functions details & get_doi from uncolorization task (v1 model)

--- src/apis/image/image/uncolorizations/v1/v1.py
+++ src/apis/image/image/uncolorizations/v1/v1.py
@@ -1,4 +1,3 @@
-import io
 import cv2
 import numpy as np
 
@@ -10,19 +9,3 @@ def predict(image):
     res, im_png = cv2.imencode(".png", gray_image)
 
     return im_png
-
-
-def details():
-    details = {
-        "doi": "10.1371/journal.pone.0029740",
-        "example_figure": "https://camo.githubusercontent.com/5eb8b4f1f63dbdbb5c30afb10575d6ebe24bb0a156e6b81296c8191183f33edf/68747470733a2f2f692e6962622e636f2f3559304d3258622f6578616d706c652e706e67",
-        "description": "Image Uncolorization will vintage your picture to turn them into black and white style.",
-    }
-    details += get_doi(details["doi"])
-    return details
-
-
-def get_doi(doi):
-    crossref_url = f"http://api.crossref.org/works/{doi}"
-    req = requests.get(crossref_url)
-    return req.content
