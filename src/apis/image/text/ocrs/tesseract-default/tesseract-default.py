31e1ca8a072c84fc864a473c10f0348e2de5806b
Valentin De Matos <valentin.de-matos@epitech.eu>
2022-03-15 15:45:59 +0100

    doc: type hinting and docstring added to tesseract-default in ocr task

--- src/apis/image/text/ocrs/tesseract-default/tesseract-default.py
+++ src/apis/image/text/ocrs/tesseract-default/tesseract-default.py
@@ -4,19 +4,34 @@ import pytesseract
 import numpy as np
 
 
-def bytes_image_to_gray_cv2_image(image):
-    img_np = cv2.imdecode(
+def bytes_image_to_gray_np_image(image: bytes) -> np.ndarray:
+    """
+    Convert a bytes image to a gray image stored in a np.ndarray
+
+    :param image: image to convert
+    :return: gray image as a np.ndarray
+    """
+
+    np_image = cv2.imdecode(
         buf=np.fromstring(image, np.uint8),
         flags=cv2.IMREAD_COLOR
     )
 
-    return cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY)
+    return cv2.cvtColor(np_image, cv2.COLOR_BGR2GRAY)
 
 
-def predict(image, source_language):
+def predict(image: bytes, source_language: str) -> [str]:
+    """
+    Call the tesseract ocr and return the text detected in the image
+
+    :param image: image to provide to the ocr
+    :param source_language: [UNUSED] language of the text to be searched
+    :return: characters found in the image
+    """
+
     del source_language
 
-    gray_image = bytes_image_to_gray_cv2_image(image)
+    gray_image = bytes_image_to_gray_np_image(image)
 
     gray_thresh = cv2.threshold(
         src=gray_image,
