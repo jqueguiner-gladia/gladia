09156551ccef9b082732979c7830bfc054cd0ddd
Valentin De Matos <valentin.de-matos@epitech.eu>
2022-03-15 15:11:22 +0100

    doc: type hinting and docstring added to easyocr in ocr task

--- src/apis/image/text/ocrs/easy-ocr/easy-ocr.py
+++ src/apis/image/text/ocrs/easy-ocr/easy-ocr.py
@@ -1,9 +1,16 @@
 import easyocr
 
 
-def predict(image, source_language):
+def predict(image: bytes, source_language: str) -> [str]:
+    """
+    Call the EasyOcr package and return the text detected in the image by the ocr
+
+    :param image: image to provide to the ocr
+    :param source_language: language of the text to be searched
+    :return: characters found in the image
+    """
 
     reader = easyocr.Reader([source_language], gpu=True)
     text = reader.readtext(image, detail=False)
-    
+
     return text
