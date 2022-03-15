21aa6e51a74b0e7f85e07174168ce9ca5fe8c4a9
Valentin De Matos <valentin.de-matos@epitech.eu>
2022-03-15 14:59:14 +0100

    refactor: fixed (PEP 8)E251 in easy-ocr (ocr task)

--- src/apis/image/text/ocrs/easy-ocr/easy-ocr.py
+++ src/apis/image/text/ocrs/easy-ocr/easy-ocr.py
@@ -3,7 +3,7 @@ import easyocr
 
 def predict(image, source_language):
 
-    reader = easyocr.Reader([source_language], gpu = True)
-    text = reader.readtext(image, detail = 0)
+    reader = easyocr.Reader([source_language], gp=True)
+    text = reader.readtext(image, detail=0)
     
     return text
