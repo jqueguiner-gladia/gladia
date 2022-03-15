147fa440f3920d7808db3ae3c08fb49e75c57f9d
Valentin De Matos <valentin.de-matos@epitech.eu>
2022-03-15 15:30:44 +0100

    refactor: tesseract-default in ocr task is now splitted in two functions
    * unused imports deleted
    * create bytes_image_to_gray_cv2_image to be more clearer
    * unused param is now deleted

--- src/apis/image/text/ocrs/tesseract-default/tesseract-default.py
+++ src/apis/image/text/ocrs/tesseract-default/tesseract-default.py
@@ -1,18 +1,32 @@
-import numpy as np
+import re
 import cv2
 import pytesseract
-from PIL import Image
-from icecream import ic
-import re
+import numpy as np
+
+
+def bytes_image_to_gray_cv2_image(image):
+    img_np = cv2.imdecode(
+        buf=np.fromstring(image, np.uint8),
+        flags=cv2.IMREAD_COLOR
+    )
+
+    return cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY)
+
 
 def predict(image, source_language):
-    nparr = np.fromstring(image, np.uint8)
-    img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR) 
-    gray = cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY)
-    
-    gray_thresh = cv2.threshold(gray, 0, 255,
-        cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    del source_language
+
+    gray_image = bytes_image_to_gray_cv2_image(image)
+
+    gray_thresh = cv2.threshold(
+        src=gray_image,
+        thresh=0,
+        maxval=255,
+        type=cv2.THRESH_BINARY | cv2.THRESH_OTSU,
+    )[1]
     
     text = pytesseract.image_to_string(gray_thresh)
+
     out = re.sub(r'[\x00-\x08\x0b\x0c\x0e-\x1f\x7f-\xff]', '', text)
+
     return [out.strip()]
