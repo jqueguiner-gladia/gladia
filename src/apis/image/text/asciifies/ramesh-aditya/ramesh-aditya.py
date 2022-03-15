f6b7058e0e72dd92c215f55aa2b1b9a690acd549
Valentin De Matos <valentin.de-matos@epitech.eu>
2022-03-15 14:48:56 +0100

    refactor: ramesh-aditya model in asciify task
    * ASCII_CHARS is now longer reversed after it's creation, it's now already created in the right order
    * variables are renamed to be more understandable
    * functions are renamed to be more understandable
    * functions are now type hinted
    * functions are now documented with doc string
    * lines spacement added between the functions

--- src/apis/image/text/asciifies/ramesh-aditya/ramesh-aditya.py
+++ src/apis/image/text/asciifies/ramesh-aditya/ramesh-aditya.py
@@ -1,54 +1,63 @@
-from gladia_api_utils.io import _open
 from PIL import Image
+from gladia_api_utils.io import _open
 
-ASCII_CHARS = ['.',',',':',';','+','*','?','%','S','#','@']
-ASCII_CHARS = ASCII_CHARS[::-1]
 
-'''
-method resize():
-    - takes as parameters the image, and the final width
-    - resizes the image into the final width while maintaining aspect ratio
-'''
-def resize(image, new_width=100):
-    (old_width, old_height) = image.size
-    aspect_ratio = float(old_height)/float(old_width)
+ASCII_CHARS = ['@', '#', 'S', '%', '?', '*', '+', ';', ':', ',', '.']
+
+
+def resize_image(image: Image, new_width: int = 100) -> Image:
+    """
+    Resizes the image into the final width while maintaining aspect ratio
+
+    :param image: image to resize
+    :param new_width: target width
+    :return: image resized with the new width
+    """
+
+    (width, height) = image.size
+
+    aspect_ratio = height / width
     new_height = int(aspect_ratio * new_width)
-    new_dim = (new_width, new_height)
-    new_image = image.resize(new_dim)
+
+    new_image = image.resize((new_width, new_height))
+
     return new_image
-'''
-method grayscalify():
-    - takes an image as a parameter
-    - returns the grayscale version of image
-'''
-def grayscalify(image):
-    return image.convert('L')
 
-'''
-method modify():
-    - replaces every pixel with a character whose intensity is similar
-'''
-def modify(image, buckets=25):
+
+def convert_image_to_ascii(image: Image, buckets: int = 255 // (len(ASCII_CHARS) - 1)) -> str:
+    """
+    Replace every pixel with a character whose intensity is similar
+
+    :param image: image to convert to ascii
+    :param buckets: range of pixels associated to each ascii character
+    :return: the ascii image converted to ascii characters
+    """
+
+    (width, _) = image.size
+
     initial_pixels = list(image.getdata())
-    new_pixels = [ASCII_CHARS[pixel_value//buckets] for pixel_value in initial_pixels]
-    return ''.join(new_pixels)
 
-'''
-method do():
-    - does all the work by calling all the above functions
-'''
-def do(image, new_width=100):
-    image = resize(image)
-    image = grayscalify(image)
+    # convert pixels to ascii characters
+    ascii_characters = ''.join([ASCII_CHARS[pixel_value // buckets] for pixel_value in initial_pixels])
 
-    pixels = modify(image)
-    len_pixels = len(pixels)
+    # Split the image in multiple list (one by row)
+    ascii_image = [ascii_characters[index:index + width] for index in range(0, len(ascii_characters), width)]
 
-    # Construct the image from the character list
-    new_image = [pixels[index:index+new_width] for index in range(0, len_pixels, new_width)]
-
-    return '\n'.join(new_image)
+    return '\n'.join(ascii_image)
 
 
-def predict(image):
-    return do(_open(image))
+def predict(image: bytes) -> str:
+    """
+    Transform an image to ascii characters
+
+    :param image: image to transform
+    :return: image in ascii characters
+    """
+
+    new_width = 100
+    image = _open(image)
+
+    image = resize_image(image, new_width=new_width)
+    new_image = convert_image_to_ascii(image.convert('L'))
+
+    return new_image
