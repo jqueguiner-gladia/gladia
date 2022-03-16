1d7b50549634765ad6e2da624b92c2e0c993f0bd
Valentin De Matos <valentin.de-matos@epitech.eu>
2022-03-16 19:24:39 +0100

    doc: type hinting & docstring for toftrup-etal-2021

--- src/apis/text/text/language-detections/toftrup-etal-2021/toftrup-etal-2021.py
+++ src/apis/text/text/language-detections/toftrup-etal-2021/toftrup-etal-2021.py
@@ -1,9 +1,19 @@
-from LanguageIdentifier import predict, rank
 import json
 
-def predict(text):
-    output = list()
-    for k,v in rank(text):
-        output.append({'language': k, 'score': v})
+from LanguageIdentifier import rank
+
+
+def predict(text: str) -> str:
+    """
+    From a given text, return a json scoring the probability of the given text to be of a certain language
+
+    :param text: text to analyze
+    :return: json scoring the chance of the text to be in each language
+    """
+
+    output = []
+
+    for lang, score in rank(text):
+        output.append({'language': lang, 'score': score})
 
     return json.dumps(output)
