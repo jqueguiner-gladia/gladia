4cff9c2697ca41471b9b2b4ef4aa569e1cec941b
Valentin De Matos <valentin.de-matos@epitech.eu>
2022-03-16 18:53:01 +0100

    refactor: predict func & var name to be more understandable in Hate-speech-CNERG-dehatebert-mono-english (hate speech reco task)

--- src/apis/text/text/hate-speech-detections/Hate-speech-CNERG-dehatebert-mono-english/Hate-speech-CNERG-dehatebert-mono-english.py
+++ src/apis/text/text/hate-speech-detections/Hate-speech-CNERG-dehatebert-mono-english/Hate-speech-CNERG-dehatebert-mono-english.py
@@ -1,27 +1,21 @@
-# https://github.com/hate-alert/HateXplain/blob/master/manual_training_inference.py
-from pydantic import BaseModel
 import torch
+
 from transformers import BertTokenizer, BertForSequenceClassification
 
 
 def predict(text):
-    # load model
-    model = BertForSequenceClassification.from_pretrained('Hate-speech-CNERG/bert-base-uncased-hatexplain')
-    tokenizer = BertTokenizer.from_pretrained('Hate-speech-CNERG/bert-base-uncased-hatexplain')
+    labels = ["hate-speech", "normal", "offensive"]
+    model_name = 'Hate-speech-CNERG/bert-base-uncased-hatexplain'
 
-    # load model into gpu
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    model = BertForSequenceClassification.from_pretrained(model_name)
     model.to(device)
 
+    tokenizer = BertTokenizer.from_pretrained(model_name)
+
     inputs = tokenizer(text, return_tensors="pt")
+
     outputs = model(**inputs)
-    label = torch.argmax(torch.nn.functional.softmax(outputs.logits, dim=1))
 
-    if (label == 0):
-        label = "hate-speech"
-    elif (label == 2):
-        label = "offensive"
-    else:
-        label = "normal"
-
-    return label
+    return labels[torch.argmax(outputs.logits, dim=1)]
