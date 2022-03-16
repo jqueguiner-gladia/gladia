897171488b6b7559ae7b97cc9f15db7b3c2a3b05
Valentin De Matos <valentin.de-matos@epitech.eu>
2022-03-16 19:43:35 +0100

    doc: type hinting & docstring for EleutherAI-gpt-neo-2_7B

--- src/apis/text/text/language-generations/EleutherAI-gpt-neo-2_7B/EleutherAI-gpt-neo-2_7B.py
+++ src/apis/text/text/language-generations/EleutherAI-gpt-neo-2_7B/EleutherAI-gpt-neo-2_7B.py
@@ -1,6 +1,14 @@
 from transformers import pipeline
 
-def predict(text):
+
+def predict(text: str) -> str:
+    """
+    Generate the continuation of the sentence
+
+    :param text: sentence to continue
+    :return: continuation of the sentence
+    """
+
     generator = pipeline('text-generation', model='EleutherAI/gpt-neo-2.7B')
 
     res = generator(text, max_length=50, do_sample=True, temperature=0.9)
