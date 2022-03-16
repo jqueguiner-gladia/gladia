430846a970b985e08631b2588309e02dd1cc818f
Valentin De Matos <valentin.de-matos@epitech.eu>
2022-03-16 16:44:20 +0100

    doc: type hinting and docstring in dbmdz-bert-large-cased-finetuned-conll03-english (ner task)

--- src/apis/text/text/entity-extractions/dbmdz-bert-large-cased-finetuned-conll03-english/dbmdz-bert-large-cased-finetuned-conll03-english.py
+++ src/apis/text/text/entity-extractions/dbmdz-bert-large-cased-finetuned-conll03-english/dbmdz-bert-large-cased-finetuned-conll03-english.py
@@ -1,7 +1,13 @@
 from transformers import pipeline
 
 
-def predict(input_string):
+def predict(input_string: str) -> [dict]:
+    """
+    Extract the named entity from a given string
+
+    :param input_string: string to extract the entities from
+    :return: entities founded in the string
+    """
 
     ner_pipeline = pipeline("ner")
     entities = ner_pipeline(input_string)
