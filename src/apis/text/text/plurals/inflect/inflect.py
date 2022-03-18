ec3c9e9e5d4907b35c14b169437c3ba7b7e0434f
Valentin De Matos <valentin.de-matos@epitech.eu>
2022-03-18 20:52:24 +0100

    doc: type hinting & docstring for plural task

--- src/apis/text/text/plurals/inflect/inflect.py
+++ src/apis/text/text/plurals/inflect/inflect.py
@@ -1,5 +1,21 @@
 import inflect
 
-def predict(word, count):
+
+def predict(word: str, count: int) -> str:
+    """
+    Apply the `inflect.engine().plural` function to accord the `word` in plural regarding its `count`.\n
+
+    As specified in `infect` documentation, the function return as follows:\n
+
+    If count is one of the following words, then return text:
+        1, a, an, one, each, every, this, that
+
+
+    :param word: word to return the plural of
+    :param count: count of `word`
+    :return: word accorded with count
+    """
+
     p = inflect.engine()
+
     return p.plural(word, count)
