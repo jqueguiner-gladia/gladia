f87109352b04b72c07c54ab618d5920819c93017
Valentin De Matos <valentin.de-matos@epitech.eu>
2022-03-18 21:22:04 +0100

    doc: type hinting & docstring in sentdex-GPyT (prog lang generation)

--- src/apis/text/text/programming-language-generations/sentdex-GPyT/sentdex-GPyT.py
+++ src/apis/text/text/programming-language-generations/sentdex-GPyT/sentdex-GPyT.py
@@ -1,29 +1,47 @@
 import torch
 
-from transformers import AutoTokenizer, AutoModelWithLMHead
+from transformers import AutoTokenizer, AutoModelWithLMHead, PreTrainedTokenizer, PreTrainedModel
 
 
-def generate(code, tokenizer, model, max_length=100):
+def generate(code: str, tokenizer: PreTrainedTokenizer, model: PreTrainedModel, max_length: int = 100) -> str:
+    """
+    Takes input code, replaces newline chars with <N>,
+    tokenizes, feeds through model, decodes, then reformat the newlines back in.
+
+    :param code: code to continue
+    :param tokenizer: tokenizer to use
+    :param model: model to use
+    :param max_length: maximum length to continue up to
+    :return: generated code
+    """
+
+    new_line_token = "<N>"
+
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    '''Takes input code, replaces newline chars with <N>, 
-    tokenizes, feeds thru model, decodes, 
-    then reformats the newlines back in'''
-    newlinechar = "<N>"
-
     model = model.to(device)
 
-    converted = code.replace("\n", newlinechar)
+    converted = code.replace("\n", new_line_token)
+
     tokenized = tokenizer.encode(converted, return_tensors='pt').to(device)
     resp = model.generate(tokenized, max_length=max_length).to(device)
 
     decoded = tokenizer.decode(resp[0])
-    reformatted = decoded.replace("<N>", "\n")
+    reformatted = decoded.replace(new_line_token, "\n")
+
     return reformatted
 
 
-def predict(code_snippet):
+def predict(code_snippet: str) -> str:
+    """
+    Generate the continuation of the provided `code_snippet`.
 
-    tokenizer = AutoTokenizer.from_pretrained("Sentdex/GPyT")
-    model = AutoModelWithLMHead.from_pretrained("Sentdex/GPyT")
+    :param code_snippet:
+    :return:
+    """
+
+    model_name = "Sentdex/GPyT"
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelWithLMHead.from_pretrained(model_name)
 
     return generate(code_snippet, tokenizer, model)
