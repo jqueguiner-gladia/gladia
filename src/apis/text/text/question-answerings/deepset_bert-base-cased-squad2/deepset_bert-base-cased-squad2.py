35b8b179f25e779d85f845409518b0c84c608fe3
Valentin De Matos <valentin.de-matos@epitech.eu>
2022-03-19 10:07:48 +0100

    doc: type hinting & docstring for deepset_bert-base-cased-squad2 (SQUAD task)

--- src/apis/text/text/question-answerings/deepset_bert-base-cased-squad2/deepset_bert-base-cased-squad2.py
+++ src/apis/text/text/question-answerings/deepset_bert-base-cased-squad2/deepset_bert-base-cased-squad2.py
@@ -4,6 +4,12 @@ from transformers import AutoTokenizer, AutoModelForQuestionAnswering, PreTraine
 
 
 def load_model() -> (PreTrainedTokenizer, PreTrainedModel):
+    """
+    Load `deepset/bert-base-cased-squad2` tokenizer and model.
+
+    :return: tuple containing both the tokenizer and the model
+    """
+
     model_name = "deepset/bert-base-cased-squad2"
 
     tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -12,14 +18,22 @@ def load_model() -> (PreTrainedTokenizer, PreTrainedModel):
     return tokenizer, model
     
 
-def predict(context, question):
+def predict(context: str, question: str) -> str:
+    """
+    Using the given `context`, answer the provided `question`.
+
+    Note: The score is not provided in the return value.
+
+    :param context: context to use to answer the question
+    :param question: question to answer
+    :return: answer of the question
+    """
 
     tokenizer, model = load_model()
 
     inputs = tokenizer.encode_plus(question, context, add_special_tokens=True, return_tensors="pt")
     input_ids = inputs["input_ids"].tolist()[0]
 
-    text_tokens = tokenizer.convert_ids_to_tokens(input_ids)
     answer_start_scores, answer_end_scores = model(**inputs, return_dict=False)
 
     answer_start = torch.argmax(answer_start_scores)
