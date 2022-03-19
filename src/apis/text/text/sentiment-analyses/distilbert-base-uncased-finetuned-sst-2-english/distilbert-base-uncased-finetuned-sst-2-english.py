4148eeac1f9258515b4874d15b845651943b18f6
Valentin De Matos <valentin.de-matos@epitech.eu>
2022-03-19 17:57:59 +0100

    doc: type hinting & docstring for both distilbert-base-uncased & distilbert-base-uncased-finetuned-sst-2-english (sentiment-analyse)

--- src/apis/text/text/sentiment-analyses/distilbert-base-uncased-finetuned-sst-2-english/distilbert-base-uncased-finetuned-sst-2-english.py
+++ src/apis/text/text/sentiment-analyses/distilbert-base-uncased-finetuned-sst-2-english/distilbert-base-uncased-finetuned-sst-2-english.py
@@ -3,8 +3,18 @@ import json
 from happytransformer import HappyTextClassification
 
 
-def predict(text):
+def predict(text: str) -> str:
+    """
+    For a given text, predict if it's POSITIVE or NEGATIVE
+
+    :param text: text to analyze.
+    :return: JSON formatted str containing the label (POSITIVE/NEGATIVE) with it score
+    """
+
     happy_tc = HappyTextClassification(model_type="DISTILBERT",  model_name="distilbert-base-uncased-finetuned-sst-2-english")
     result = happy_tc.classify_text(text)
 
-    return json.dumps({"label": result.label, "score": result.score})
+    return json.dumps({
+        "label": result.label,
+        "score": result.score,
+    })
--- src/apis/text/text/sentiment-analyses/distilbert-base-uncased/distilbert-base-uncased.py
+++ src/apis/text/text/sentiment-analyses/distilbert-base-uncased/distilbert-base-uncased.py
@@ -3,8 +3,18 @@ import json
 from happytransformer import HappyTextClassification
 
 
-def predict(text):
-    happy_tc = HappyTextClassification("DISTILBERT", "distilbert-base-uncased", num_labels=2)    
+def predict(text: str) -> str:
+    """
+    For a given text, predict if it's POSITIVE or NEGATIVE
+
+    :param text: text to analyze.
+    :return: JSON formatted str containing the label (POSITIVE/NEGATIVE) with it score
+    """
+
+    happy_tc = HappyTextClassification("DISTILBERT", "distilbert-base-uncased", num_labels=2)
     result = happy_tc.classify_text(text)
 
-    return json.dumps({"label": "POSITIVE" if result.label == "LABEL_0"  else "NEGATIVE", "score": result.score})
+    return json.dumps({
+        "label": "POSITIVE" if result.label == "LABEL_0" else "NEGATIVE",
+        "score": result.score,
+    })
