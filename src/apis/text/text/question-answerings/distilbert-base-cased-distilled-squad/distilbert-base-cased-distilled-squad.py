8a231e04d34c131be76b2dfac03e5f1f35e6e45f
Valentin De Matos <valentin.de-matos@epitech.eu>
2022-03-19 09:44:36 +0100

    delete: unused print in models based on happytransformer for SQUAD task

--- src/apis/text/text/question-answerings/distilbert-base-cased-distilled-squad/distilbert-base-cased-distilled-squad.py
+++ src/apis/text/text/question-answerings/distilbert-base-cased-distilled-squad/distilbert-base-cased-distilled-squad.py
@@ -1,12 +1,11 @@
-from happytransformer import HappyQuestionAnswering
 import json
+
+from happytransformer import HappyQuestionAnswering
+
+
 def predict(context, question):
     
     happy_qa = HappyQuestionAnswering("DISTILBERT", "distilbert-base-cased-distilled-squad")
     result = happy_qa.answer_question(context, question)
-    print(type(result))  # <class 'list'>
-    print(result)  # [QuestionAnsweringResult(answer='January 10th, 2021', score=0.9711642265319824, start=16, end=34)]
-    print(type(result[0]))  # <class 'happytransformer.happy_question_answering.QuestionAnsweringResult'>
-    print(result[0])  # QuestionAnsweringResult(answer='January 10th, 2021', score=0.9711642265319824, start=16, end=34)
-    print(result[0].answer)  # January 10th, 2021
+
     return json.dumps({'answer': result[0].answer, 'score': result[0].score})
