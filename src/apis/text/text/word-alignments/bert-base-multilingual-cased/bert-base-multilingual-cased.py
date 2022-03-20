74d8b2d7860c8364059e71a0072351c016309349
Valentin De Matos <valentin.de-matos@epitech.eu>
2022-03-20 15:12:09 +0100

    refactor: bert-base-multilingual-cased model (word alignment)
    * slited predict into multiple functions
    * renamed var to be more comprehensive
    * type hinted functins
    * add docstring to each functions

--- src/apis/text/text/word-alignments/bert-base-multilingual-cased/bert-base-multilingual-cased.py
+++ src/apis/text/text/word-alignments/bert-base-multilingual-cased/bert-base-multilingual-cased.py
@@ -1,50 +1,130 @@
-import transformers
 import torch
 import itertools
+import transformers
 
-#https://colab.research.google.com/drive/1205ubqebM0OsZa1nRgbGJBtitgHqIVv6?usp=sharing#scrollTo=smW6s5JJflCN
 
-def predict(input_string_language_1, input_string_language_2):
-    model = transformers.BertModel.from_pretrained('bert-base-multilingual-cased')
-    tokenizer = transformers.BertTokenizer.from_pretrained('bert-base-multilingual-cased')
+def get_tokens(input_string: str, tokenizer: transformers.PreTrainedTokenizer) -> ([str], [[str]]):
+    """
+    Tokenize an input string
 
-    sent_src, sent_tgt = input_string_language_1.strip().split(), input_string_language_2.strip().split()
-    token_src, token_tgt = [tokenizer.tokenize(word) for word in sent_src], [tokenizer.tokenize(word) for word in sent_tgt]
-    wid_src, wid_tgt = [tokenizer.convert_tokens_to_ids(x) for x in token_src], [tokenizer.convert_tokens_to_ids(x) for x in token_tgt]
-    ids_src, ids_tgt = tokenizer.prepare_for_model(list(itertools.chain(*wid_src)), return_tensors='pt', model_max_length=tokenizer.model_max_length, truncation=True)['input_ids'], tokenizer.prepare_for_model(list(itertools.chain(*wid_tgt)), return_tensors='pt', truncation=True, model_max_length=tokenizer.model_max_length)['input_ids']
-    sub2word_map_src = []
+    :param input_string: string to tokenize
+    :param tokenizer: tokenizer to use
+    :return: tuple contained the string splited by space and the tokenized string
+    """
 
-    for i, word_list in enumerate(token_src):
-        sub2word_map_src += [i for x in word_list]
-    sub2word_map_tgt = []
+    sentence = input_string.strip().split()
+    token = [tokenizer.tokenize(word) for word in sentence]
 
-    for i, word_list in enumerate(token_tgt):
-        sub2word_map_tgt += [i for x in word_list]
+    return sentence, token
 
-    # alignment
-    align_layer = 8
-    threshold = 1e-3
-    model.eval()
 
-    with torch.no_grad():
-        out_src = model(ids_src.unsqueeze(0), output_hidden_states=True)[2][align_layer][0, 1:-1]
-        out_tgt = model(ids_tgt.unsqueeze(0), output_hidden_states=True)[2][align_layer][0, 1:-1]
+def tokens_to_tensor(tokens: [[str]], tokenizer: transformers.PreTrainedTokenizer) -> torch.Tensor:
+    """
+    From a given tokenized sentence, return it as a Tensor
 
-    dot_prod = torch.matmul(out_src, out_tgt.transpose(-1, -2))
+    :param tokens: tokens to transform
+    :param tokenizer: tokenizer used to tokenize the text
+    :return: tensor containing the tokens
+    """
 
-    softmax_srctgt = torch.nn.Softmax(dim=-1)(dot_prod)
-    softmax_tgtsrc = torch.nn.Softmax(dim=-2)(dot_prod)
+    ids = [tokenizer.convert_tokens_to_ids(t) for t in tokens]
 
-    softmax_inter = (softmax_srctgt > threshold)*(softmax_tgtsrc > threshold)
+    prepared_ids = tokenizer.prepare_for_model(
+        ids=list(itertools.chain(*ids)),
+        return_tensors='pt',
+        model_max_length=tokenizer.model_max_length,
+        truncation=True
+    )
 
-    align_subwords = torch.nonzero(softmax_inter, as_tuple=False)
+    return prepared_ids['input_ids']
+
+
+def map_tokens_to_words(tokens: [[str]]) -> [int]:
+    """
+    Associate tokens to their respective words.
+
+    :param tokens: tokens to associate to
+    :return: word index for which the token come from
+    """
+
+    tok2word_map = []
+
+    for i, word_list in enumerate(tokens):
+        tok2word_map += [i] * len(word_list)
+
+    return tok2word_map
+
+
+def get_words_alignment(
+        sentence_src: [str],
+        sentence_tgt: [str],
+        tok2word_map_src: [int],
+        tok2word_map_tgt: [int],
+        softmax_inter: torch.Tensor,
+) -> [dict]:
+    """
+    Associate words from sentence_src to sentence_tgt
+
+    :param sentence_src: splited source sentence
+    :param sentence_tgt: splited target sentence
+    :param tok2word_map_src: map associating tokens to their words for source sentence
+    :param tok2word_map_tgt: map associating tokens to their words for target sentence
+    :param softmax_inter: tell whether or to not add each word
+    :return: word alignment from source to target
+    """
+
     align_words = set()
-    for i, j in align_subwords:
-        align_words.add( (sub2word_map_src[i], sub2word_map_tgt[j]) )
-
+    for i, j in torch.nonzero(softmax_inter, as_tuple=False):
+        align_words.add((tok2word_map_src[i], tok2word_map_tgt[j]))
 
     output = []
     for i, j in sorted(align_words):
-        output.append({"source": sent_src[i], "target": sent_tgt[j]})
+        output.append({"source": sentence_src[i], "target": sentence_tgt[j]})
 
     return output
+
+
+def predict(input_string_language_1: str, input_string_language_2: str) -> [dict]:
+    """
+    Associated words from `input_string_language_1` to `input_string_language_2`.
+
+    :param input_string_language_1: sentence to associate from
+    :param input_string_language_2: sentence to associate to
+    :return: list of dict associated each word from source to targe
+    """
+
+    threshold = 1E-3
+    align_layer = 8
+    model_name = 'bert-base-multilingual-cased'
+
+    model = transformers.BertModel.from_pretrained(model_name)
+    tokenizer = transformers.BertTokenizer.from_pretrained(model_name)
+
+    model.eval()
+
+    sentence_src, tokens_src = get_tokens(input_string_language_1, tokenizer)
+    sentence_tgt, tokens_tgt = get_tokens(input_string_language_2, tokenizer)
+
+    tensor_src = tokens_to_tensor(tokens=tokens_src, tokenizer=tokenizer)
+    tensor_tgt = tokens_to_tensor(tokens=tokens_tgt, tokenizer=tokenizer)
+
+    tok2word_map_src = map_tokens_to_words(tokens_src)
+    tok2word_map_tgt = map_tokens_to_words(tokens_tgt)
+
+    with torch.no_grad():
+        out_src = model(tensor_src.unsqueeze(0), output_hidden_states=True)[2][align_layer][0, 1:-1]
+        out_tgt = model(tensor_tgt.unsqueeze(0), output_hidden_states=True)[2][align_layer][0, 1:-1]
+
+    del tensor_src, tensor_tgt
+
+    dot_prod = torch.matmul(out_src, out_tgt.transpose(-1, -2))
+
+    softmax_inter = (torch.nn.Softmax(dim=-1)(dot_prod) > threshold) * (torch.nn.Softmax(dim=-2)(dot_prod) > threshold)
+
+    return get_words_alignment(
+        sentence_src=sentence_src,
+        sentence_tgt=sentence_tgt,
+        tok2word_map_src=tok2word_map_src,
+        tok2word_map_tgt=tok2word_map_tgt,
+        softmax_inter=softmax_inter,
+    )
