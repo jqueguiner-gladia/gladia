6e78a37b6afc28e76f4c535f4a37f6ac93572bb7
Valentin De Matos <valentin.de-matos@epitech.eu>
2022-04-29 16:26:00 +0200

    fix: ocrs now import tesseract in the predict function

--- src/apis/image/text/ocrs/tesseract-default/tesseract-default.py
+++ src/apis/image/text/ocrs/tesseract-default/tesseract-default.py
@@ -1,6 +1,5 @@
 import re
 import cv2
-import pytesseract
 import numpy as np
 
 from gladia_api_utils.io import _open
@@ -14,6 +13,8 @@ def predict(image: bytes, source_language: str) -> [str]:
     :param source_language: [UNUSED] language of the text to be searched
     :return: characters found in the image
     """
+
+    import pytesseract
 
     del source_language
 
