f92be4d86a25c3e562574af041bc24f1fe360468
Valentin De Matos <valentin.de-matos@epitech.eu>
2022-05-10 12:26:51 +0200

    fix: typo varenv TRITON_SEVER_URL -> TRITON_SERVER_URL

--- src/apis/text/text/hate-speech-detections/Hate-speech-CNERG-dehatebert-mono-english/Hate-speech-CNERG-dehatebert-mono-english.py
+++ src/apis/text/text/hate-speech-detections/Hate-speech-CNERG-dehatebert-mono-english/Hate-speech-CNERG-dehatebert-mono-english.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-import tritonclient.http as tritonhttpclient
+import tritonclient.http as tritonclient
 
 from transformers import BertTokenizer
 
@@ -16,20 +16,20 @@ def predict(text: str) -> str:
     LABELS = ["hate-speech", "normal", "offensive"]
     MODEL_NAME = "bert-base-uncased-hatexplain"
     TOKENIZER_NAME = 'Hate-speech-CNERG/bert-base-uncased-hatexplain'
-    TRITON_SEVER_URL = os.getenv("TRITON_SEVER_URL", default='localhost:8000')
+    TRITON_SEVER_URL = os.getenv("TRITON_SERVER_URL", default='localhost:8000')
 
-    triton_client = tritonhttpclient.InferenceServerClient(url=TRITON_SEVER_URL, verbose=False)
+    client = tritonclient.InferenceServerClient(url=TRITON_SEVER_URL, verbose=False)
 
     tokenizer = BertTokenizer.from_pretrained(TOKENIZER_NAME)
 
     input_ids = tokenizer(text, return_tensors="pt", max_length=256, padding="max_length").input_ids
 
-    input0 = tritonhttpclient.InferInput('input__0', (1, 256), 'INT32')
-    input0.set_data_from_numpy(input_ids.detach().numpy().astype(np.int32), binary_data=False)
+    input0 = tritonclient.InferInput('input__0', (1, 256), 'INT32')
+    input0.set_data_from_numpy(input_ids.detach().numpy().astype(np.int32))
 
-    output0 = tritonhttpclient.InferRequestedOutput('output__0',  binary_data=False)
+    output0 = tritonclient.InferRequestedOutput('output__0')
 
-    response = triton_client.infer(MODEL_NAME, model_version='1', inputs=[input0], outputs=[output0])
+    response = client.infer(MODEL_NAME, model_version='1', inputs=[input0], outputs=[output0])
     output = response.as_numpy("output__0")[0]
 
     return LABELS[np.argmax(output)]
