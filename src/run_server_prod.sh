1bb6565a1812657fa20085e048d724f3889aece2
Valentin De Matos <valentin.de-matos@epitech.eu>
2022-05-16 17:05:58 +0200

    !refactor: run_server_prod now also launch warm_up and triton server

--- src/run_server_prod.sh
+++ src/run_server_prod.sh
@@ -1,1 +1,17 @@
-uvicorn main:app --host 0.0.0.0 --port 80 --workers 8
+python warm_up.py &&
+docker run \                             
+  --gpus=1 \
+  --ipc=host --rm \
+  --shm-size=1g \
+  --ulimit memlock=-1 \
+  --ulimit stack=67108864 \
+  -p ${TRITON_SERVER_PORT}:8000 -p 8001:8001 -p 8002:8002 \
+  -v $PWD/triton:/models \
+  nvcr.io/nvidia/tritonserver:22.04-py3 \
+  tritonserver \
+  --model-repository=${TRITON_MODELS_PATH}
+  --exit-on-error=false \
+  --model-control-mode=explicit \
+  --repository-poll-secs 10 \
+    --allow-metrics=false \
+& uvicorn main:app --host 0.0.0.0 --port 80 --workers 8
