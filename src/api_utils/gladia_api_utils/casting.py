f178c5811f5993919c2cd094d8c04e9703062e65
Valentin De Matos <valentin.de-matos@epitech.eu>
2022-05-17 17:06:39 +0200

    delete: unused NumpyEncoder

--- src/api_utils/gladia_api_utils/casting.py
+++ src/api_utils/gladia_api_utils/casting.py
@@ -12,13 +12,6 @@ from fastapi.encoders import jsonable_encoder
 from starlette.responses import StreamingResponse
 
 
-class NumpyEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, np.ndarray):
-            return obj.tolist()
-        return json.JSONEncoder.default(self, obj)
-
-
 class NpEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, np.integer):
