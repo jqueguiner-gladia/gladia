da98b3b7124b49d7f1a70125c2422a2778b377ed
Valentin De Matos <valentin.de-matos@epitech.eu>
2022-05-18 08:53:17 +0200

    refactor: renamed __convert_PIL_Image_response to __convert_pillow_image_response

--- src/api_utils/gladia_api_utils/casting.py
+++ src/api_utils/gladia_api_utils/casting.py
@@ -24,7 +24,7 @@ class NpEncoder(json.JSONEncoder):
             return super(NpEncoder, self).default(obj)
 
 
-def __convert_PIL_Image_response(response: PIL.Image.Image):
+def __convert_pillow_image_response(response: PIL.Image.Image):
     ioresult = io.BytesIO()
 
     response.save(ioresult, format="png")
@@ -109,7 +109,7 @@ def cast_response(response, expected_output: dict):
     """
 
     if isinstance(response, PIL.Image.Image):
-        return __convert_PIL_Image_response(response)
+        return __convert_pillow_image_response(response)
 
     elif isinstance(response, np.ndarray):
         return __convert_ndarray_response(response, expected_output["type"])
