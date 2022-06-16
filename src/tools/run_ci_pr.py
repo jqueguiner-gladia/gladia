19ec3f0c17af702c656145663f13e11ac459278a
Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>
2022-06-16 06:59:39 +0200

    fix sha short reference

--- src/tools/run_ci_pr.py
+++ src/tools/run_ci_pr.py
@@ -5,6 +5,17 @@ import click
 import sys
 import os
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 # using an access token
 @click.command()
 @click.option('--commit_short', default="", required=True, help='Short Sha of commit.')
@@ -19,21 +30,26 @@ def commit_should_run(commit_short="", repo="", gh_token=""):
   #0 is ok
   #1 is nok
   skip_build = True
+  print(commit_short)
   for pr in pulls:
     if "WIP" not in pr.title:    
       pr_number = pr.number
-      print(f"looking into {pr.title}")
+      print(f"- looking into {pr.title}")
       req = requests.get(f'https://api.github.com/repos/gladiaio/gladia/pulls/{pr_number}/commits')
       commits = json.loads(req.text)
       for commit in commits:
-        if commit["sha"][0:6] == commit_short:
+        
+        if commit["sha"][0:7] == commit_short:
           skip_build = False
+          print(" |_ " + bcolors.OKGREEN + commit['sha'][0:7] + bcolors.ENDC)
+        else:
+          print(f" |_ {commit['sha'][0:7]}")
 
   if skip_build:
-    print("Shouldn't run")
+    print("CI Shouldn't run")
     sys.exit(1)
   else:
-    print("should run")
+    print("CI Should run")
     sys.exit(0)
 
   return not skip_build
