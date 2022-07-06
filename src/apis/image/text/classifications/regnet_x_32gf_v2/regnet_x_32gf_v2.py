f999a8952db19e8deab90390923766fa6331c4cc
Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>
2022-07-06 23:55:44 +0200

    feat(regnet_x_32gf): add regnet_x_32gf_v1 and regnet_x_32gf_v2 to image/text/classification (#347)
    * feat(regnet_x_1_6gf): add regnet_x_1_6gf_v1 and regnet_x_1_6gf_v2 to image/text/classification
    
    * feat(regnet_x_16gf): add regnet_x_16gf_v1 and regnet_x_16gf_2 to image/text/classification
    
    * feat(regnet_x_32gf): Add regnet_x_32gf_v1 and regnet_x_32gf_v2 to image/text/classification

---
+++ src/apis/image/text/classifications/regnet_x_32gf_v2/regnet_x_32gf_v2.py
@@ -0,0 +1,15 @@
+from gladia_api_utils.TorchvisionModelHelper import TorchvisionModel
+from gladia_api_utils.io import _open
+
+
+def predict(image: bytes, top_k: int = 1) -> [str]:
+    img = _open(image)
+
+    model = TorchvisionModel(
+        model_name="regnet_x_32gf",
+        weights="RegNet_X_32GF_Weights",
+        weights_version="IMAGENET1K_V2",
+    )
+    output = model(img, top_k)
+
+    return output
