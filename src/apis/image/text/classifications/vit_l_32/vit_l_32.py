edbda42267746716b7f586469b7720c4552e9f5b
Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>
2022-07-07 17:16:08 +0200

    feat(vit_l_32): add ViT_L_32 to image/text/classification (#367)
    Signed-off-by: Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>

---
+++ src/apis/image/text/classifications/vit_l_32/vit_l_32.py
@@ -0,0 +1,11 @@
+from gladia_api_utils.TorchvisionModelHelper import TorchvisionModel
+from gladia_api_utils.io import _open
+
+
+def predict(image: bytes, top_k: int = 1) -> [str]:
+    img = _open(image)
+
+    model = TorchvisionModel(model_name="vit_l_32", weights="ViT_L_32_Weights")
+    output = model(img, top_k)
+
+    return output
