9d8a947641d1241b582e1ae95d91e7b9d641be43
Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>
2022-07-07 07:49:07 +0200

    feat(vgg13_bn): add VGG13_BN to image/text/classification (#371)
    Signed-off-by: Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>

---
+++ src/apis/image/text/classifications/vgg13_bn/vgg13_bn.py
@@ -0,0 +1,11 @@
+from gladia_api_utils.TorchvisionModelHelper import TorchvisionModel
+from gladia_api_utils.io import _open
+
+
+def predict(image: bytes, top_k: int = 1) -> [str]:
+    img = _open(image)
+
+    model = TorchvisionModel(model_name="vgg13_bn", weights="VGG13_BN_Weights")
+    output = model(img, top_k)
+
+    return output
