d1d8d690ea6c9fec67b999ea9ee9927fc954260e
Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>
2022-07-07 17:44:50 +0200

    feat(squeezenet1_1): add squeezenet1_1 to image/text/classification (#360)

---
+++ src/apis/image/text/classifications/squeezenet1_1/squeezenet1_1.py
@@ -0,0 +1,13 @@
+from gladia_api_utils.TorchvisionModelHelper import TorchvisionModel
+from gladia_api_utils.io import _open
+
+
+def predict(image: bytes, top_k: int = 1) -> [str]:
+    img = _open(image)
+
+    model = TorchvisionModel(
+        model_name="squeezenet1_1", weights="SqueezeNet1_1_Weights"
+    )
+    output = model(img, top_k)
+
+    return output
