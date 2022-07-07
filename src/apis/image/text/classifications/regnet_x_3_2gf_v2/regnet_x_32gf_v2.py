041f373f148fbd6e605b2dc2a6a194d05054ac78
Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>
2022-07-07 06:33:14 +0200

    Format/code reformatting (#350)
    * feat(regnet_x_3_2gf): add regnet_x_3_2gf_v1 and regnet_x_3_2gf_2 to image/text/classification
    
    * feat(regnet_x_400mf): add regnet_x_400mf_v1 and regnet_x_400mf_v2 for image/text/classification

---
+++ src/apis/image/text/classifications/regnet_x_3_2gf_v2/regnet_x_32gf_v2.py
@@ -0,0 +1,15 @@
+from gladia_api_utils.TorchvisionModelHelper import TorchvisionModel
+from gladia_api_utils.io import _open
+
+
+def predict(image: bytes, top_k: int = 1) -> [str]:
+    img = _open(image)
+
+    model = TorchvisionModel(
+        model_name="regnet_x_3_2gf",
+        weights="RegNet_X_3_2GF_Weights",
+        weights_version="IMAGENET1K_V2",
+    )
+    output = model(img, top_k)
+
+    return output
