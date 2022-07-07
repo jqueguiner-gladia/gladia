f8ec74280206ec462feb8371f161cc2c958a425d
Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>
2022-07-07 17:43:39 +0200

    feat(shufflenet_v2_x1_5): add shufflenet_v2_x1_5 to image/text/classi… (#364)
    feat(shufflenet_v2_x1_5): add shufflenet_v2_x1_5 to image/text/classification
    
    Signed-off-by: Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>

---
+++ src/apis/image/text/classifications/shufflenet_v2_x1_5/shufflenet_v2_x1_5.py
@@ -0,0 +1,13 @@
+from gladia_api_utils.TorchvisionModelHelper import TorchvisionModel
+from gladia_api_utils.io import _open
+
+
+def predict(image: bytes, top_k: int = 1) -> [str]:
+    img = _open(image)
+
+    model = TorchvisionModel(
+        model_name="shufflenet_v2_x1_5", weights="ShuffleNet_V2_X1_5_Weights"
+    )
+    output = model(img, top_k)
+
+    return output
