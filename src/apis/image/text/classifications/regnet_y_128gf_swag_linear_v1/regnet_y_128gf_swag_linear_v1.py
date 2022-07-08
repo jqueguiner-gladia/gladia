e2f8a86917f0716f5cb5eec40e321ff5ffa7604e
Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>
2022-07-08 18:05:00 +0200

    feat(regnet_y_128gf): add regnet_y_128gf to image/text/classification (#397)
    Signed-off-by: Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>

---
+++ src/apis/image/text/classifications/regnet_y_128gf_swag_linear_v1/regnet_y_128gf_swag_linear_v1.py
@@ -0,0 +1,16 @@
+from importlib_metadata import version
+from gladia_api_utils.TorchvisionModelHelper import TorchvisionModel
+from gladia_api_utils.io import _open
+
+
+def predict(image: bytes, top_k: int = 1) -> [str]:
+    img = _open(image)
+
+    model = TorchvisionModel(
+        model_name="regnet_y_128gf",
+        weights="RegNet_Y_128GF_Weights",
+        weights_version="IMAGENET1K_SWAG_LINEAR_V1",
+    )
+    output = model(img, top_k)
+
+    return output
