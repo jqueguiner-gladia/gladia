89ce8c3259ddb8aa3611708091a9a9edad3d840b
Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>
2022-07-08 12:13:48 +0200

    feat(regnet_x_800mf): add regnet_x_800mf_v1 and regnet_x_800mf_v2 to image/text/classification (#351)
    * feat(regnet_x_3_2gf): add regnet_x_3_2gf_v1 and regnet_x_3_2gf_2 to image/text/classification
    
    * feat(regnet_x_400mf): add regnet_x_400mf_v1 and regnet_x_400mf_v2 for image/text/classification
    
    * style(src): apply black formatting to src
    
    * feat(regnet_x_800mf): add regnet_x_800mf_v1 and regnet_x_800mf_v2 to image/text/classification

---
+++ src/apis/image/text/classifications/regnet_x_800mf_v2/regnet_x_800mf_v2.py
@@ -0,0 +1,15 @@
+from gladia_api_utils.TorchvisionModelHelper import TorchvisionModel
+from gladia_api_utils.io import _open
+
+
+def predict(image: bytes, top_k: int = 1) -> [str]:
+    img = _open(image)
+
+    model = TorchvisionModel(
+        model_name="regnet_x_800mf",
+        weights="RegNet_X_800MF_Weights",
+        weights_version="IMAGENET1K_V1",
+    )
+    output = model(img, top_k)
+
+    return output
