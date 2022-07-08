76547dadc7f641712de42b315128c82bf8be2a5c
Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>
2022-07-08 16:05:11 +0200

    feat(vit_l_16): add vit_l_16 to image/text/classification (#393)
    Signed-off-by: Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>

---
+++ src/apis/image/text/classifications/vit_l_16_swag_e2e_v1/vit_l_16_swag_e2e_v1.py
@@ -0,0 +1,16 @@
+from importlib_metadata import version
+from gladia_api_utils.TorchvisionModelHelper import TorchvisionModel
+from gladia_api_utils.io import _open
+
+
+def predict(image: bytes, top_k: int = 1) -> [str]:
+    img = _open(image)
+
+    model = TorchvisionModel(
+        model_name="vit_l_16",
+        weights="ViT_L_16_Weights",
+        weights_version="IMAGENET1K_SWAG_E2E_V1",
+    )
+    output = model(img, top_k)
+
+    return output
