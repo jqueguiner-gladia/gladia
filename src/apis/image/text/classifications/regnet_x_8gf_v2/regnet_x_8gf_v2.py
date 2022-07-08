46a6b86e2b399c3de6d437e7c6903c1cc370724d
Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>
2022-07-08 16:53:23 +0200

    feat(regnet_x_8gf): add regnet_x_8gf to image/text/classification (#396)
    Signed-off-by: Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>

---
+++ src/apis/image/text/classifications/regnet_x_8gf_v2/regnet_x_8gf_v2.py
@@ -0,0 +1,16 @@
+from importlib_metadata import version
+from gladia_api_utils.TorchvisionModelHelper import TorchvisionModel
+from gladia_api_utils.io import _open
+
+
+def predict(image: bytes, top_k: int = 1) -> [str]:
+    img = _open(image)
+
+    model = TorchvisionModel(
+        model_name="regnet_x_8gf",
+        weights="RegNet_X_8GF_Weights",
+        weights_version="IMAGENET1K_V2",
+    )
+    output = model(img, top_k)
+
+    return output
