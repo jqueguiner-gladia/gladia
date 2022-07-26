126aefd9ac4e49d2b857c4cec5b225509bd67348
Valentin De Matos <43698357+Thytu@users.noreply.github.com>
2022-07-26 19:54:54 +0200

    New language detection model : papluca_xlm-roberta-base-language-detection_tensorrt (#474)
    * feat(language-detection): new model papluca_xlm-roberta-base-language-detection_tensorrt_inference
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * help on xlm-roberta-base-language-detection.py (#476)
    
    * Update xlm-roberta-base-language-detection.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    
    * refactor(langauge-detection): changed xlm-roberta-base return type
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>

---
+++ src/apis/text/text/language-detections/xlm-roberta-base-language-detection/xlm-roberta-base-language-detection.py
@@ -0,0 +1,63 @@
+from typing import Any, Dict, List
+
+import numpy as np
+from gladia_api_utils.triton_helper import TritonClient, data_processing
+
+
+def softmax(x):
+    return np.exp(x) / np.sum(np.exp(x), axis=0)
+
+
+def predict(text: str) -> List[Dict[str, Any]]:
+    """
+    From a given text, return a json scoring the probability of the given text to be of a certain language
+
+    :param text: text to analyze
+    :return: json scoring the chance of the text to be in each language
+    """
+
+    MODEL_NAME = "language-detection_papluca_xlm-roberta-base-language-detection_tensorrt_inference"
+    MODEL_SUB_PARTS = [
+        "language-detection_papluca_xlm-roberta-base-language-detection_tensorrt_model",
+        "language-detection_papluca_xlm-roberta-base-language-detection_tensorrt_tokenize",
+    ]
+
+    LANGUAGES = [
+        "ja",
+        "nl",
+        "ar",
+        "pl",
+        "de",
+        "it",
+        "pt",
+        "tr",
+        "es",
+        "hi",
+        "el",
+        "ur",
+        "bg",
+        "en",
+        "fr",
+        "zh",
+        "ru",
+        "th",
+        "sw",
+        "vi",
+    ]
+
+    client = TritonClient(
+        model_name=MODEL_NAME,
+        sub_parts=MODEL_SUB_PARTS,
+        output_name="output",
+    )
+
+    np_output = data_processing.text_to_numpy(text)
+
+    client.register_new_input(name="TEXT", shape=np_output.shape, datatype="BYTES")
+
+    output = softmax(client(np_output)[0][0])
+
+    return [
+        {"language": language, "score": score}
+        for (language, score) in zip(LANGUAGES, output)
+    ]
