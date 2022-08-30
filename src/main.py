484ef344323591782cd38990e8cb4c4ca08be1fd
Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>
2022-08-30 06:25:08 +0000

    fix(main): fix import module issues

--- src/main.py
+++ src/main.py
@@ -339,7 +339,7 @@ def import_submodules(package: str = "module", recursive: bool = True) -> None:
             or __module_is_a_task(module_split, module_config)
             or __module_is_a_model(module_split)
         ) and (not __module_is_subprocess(module_file_path)):
-            __clean_package_import(module_path)
+            import_submodules(module_path)
             
         else:
             logger.debug(f"skipping {module_relative_path}")
