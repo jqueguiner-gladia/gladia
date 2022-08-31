d7a93c010ed5be337b7c7bd14ebf214e04e3a138
Antoine Morin <amorin@gladia.io>
2022-08-31 07:50:32 +0000

    add default model info on csv

--- src/unit-test/latency_analyse.py
+++ src/unit-test/latency_analyse.py
@@ -1,12 +1,25 @@
 import json
+import requests
 import csv
 from pprint import pprint
 data={}
 with open('unit-test/latency_diagnostic.json') as json_file:
     data = json.load(json_file)
 
+def get_default_models():
+    default_models = []
+    response = requests.get("https://v2-aipi.gladia.io/openapi.json")
+    endpoints = response.json()
+    for task, details in endpoints["paths"].items():
+        default_model = details["post"]["parameters"][0]["schema"]["default"]
+        default_models.append(
+            (task, default_model)
+        )
+    return default_models
+
+default_models = get_default_models()
 data_csv = []
-data_csv.append(["task", "model", "latency_with_loading (s)"])
+data_csv.append(["task", "model", "is_default_model" "latency_with_loading (s)"])
 model_latency_with_loading = {}
 jpg_latency_without_loading = []
 png_latency_without_loading = []
@@ -14,7 +27,8 @@ for task, model_dict in data.items():
     for model, value in model_dict.items():
         latency_with_loading = value["requests"][0]["duration"]
         model_latency_with_loading[model]=latency_with_loading
-        data_csv.append([task, model, latency_with_loading])
+        is_default_model = True if (task, model) in default_models else False
+        data_csv.append([task, model, is_default_model, latency_with_loading])
         for test in value["requests"]:
             if test["file"].get("image", None):
                 if test["file"]["image"][0] == "test.jpg":
