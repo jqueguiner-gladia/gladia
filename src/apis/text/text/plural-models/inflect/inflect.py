167745ded654bb0c7b223db7fb2a26faed4a5afc
Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>
2022-09-01 15:12:00 +0000

    fix(plural): fix Unexpected behaviors with plural nouns

--- src/apis/text/text/plural-models/inflect/inflect.py
+++ src/apis/text/text/plural-models/inflect/inflect.py
@@ -1,9 +1,9 @@
-from typing import Dict
+from typing import Dict, Union
 
 import inflect
 
 
-def predict(word: str, count: int) -> Dict[str, str]:
+def predict(word: str, count: Union[int, str]) -> Dict[str, str]:
     """
     Apply the `inflect.engine().plural` function to accord the `word` in plural regarding its `count`.\n
 
@@ -12,13 +12,22 @@ def predict(word: str, count: int) -> Dict[str, str]:
     If count is one of the following words, then return text:
         1, a, an, one, each, every, this, that
 
+    Args:
+        word (str): The word to be pluralized.
+        count (Union[int, str]): The count of the word.
 
-    :param word: word to return the plural of
-    :param count: count of `word`
-    :return: word accorded with count
+    Returns:
+        Dict[str, str]: The pluralized word.
     """
 
     p = inflect.engine()
-    result = p.plural(word, count)
-
+    
+    # if singular_noun is False
+    # it means it is a singular noun
+    if p.singular_noun(word) == False:
+        result = p.plural(word, count)
+    else:
+        result = word
+    
+    
     return {"prediction": result, "prediction_raw": result}
