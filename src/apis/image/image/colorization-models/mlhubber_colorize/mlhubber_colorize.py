a8d3995ee5ac31bc1acf403f0b4bc56b0736a6ad
Thytu <valentin.de-matos@epitech.eu>
2022-09-07 16:35:07 +0000

    refactor(mlhubber_colorize): code smell

--- src/apis/image/image/colorization-models/mlhubber_colorize/mlhubber_colorize.py
+++ src/apis/image/image/colorization-models/mlhubber_colorize/mlhubber_colorize.py
@@ -41,10 +41,11 @@ def predict(image: bytes) -> Image:
 
     model = load_model(MODEL_FILE)
 
+    T = 0.38
+    EPSILONG = 1e-6
+
     h_in, w_in = 256, 256
     h_out, w_out = h_in // 4, w_in // 4
-    epsilon = 1e-6
-    T = 0.38
 
     img_rows, img_cols = image.shape[:2]
 
@@ -57,28 +58,28 @@ def predict(image: bytes) -> Image:
     x_test = np.empty((1, h_in, w_in, 1), dtype=np.float32)
     x_test[0, :, :, 0] = image / 255.0
 
-    X_colorized = model.predict(x_test)
-    X_colorized = X_colorized.reshape((h_out * w_out, nb_q))
+    colorized_img = model.predict(x_test)
+    colorized_img = colorized_img.reshape((h_out * w_out, nb_q))
 
-    X_colorized = np.exp(np.log(X_colorized + epsilon) / T)
-    X_colorized = X_colorized / np.sum(X_colorized, 1)[:, np.newaxis]
+    colorized_img = np.exp(np.log(colorized_img + EPSILONG) / T)
+    colorized_img = colorized_img / np.sum(colorized_img, 1)[:, np.newaxis]
 
     q_a = q_ab[:, 0].reshape((1, 313))
     q_b = q_ab[:, 1].reshape((1, 313))
 
-    X_a = np.sum(X_colorized * q_a, 1).reshape((h_out, w_out))
-    X_b = np.sum(X_colorized * q_b, 1).reshape((h_out, w_out))
+    x_a = np.sum(colorized_img * q_a, 1).reshape((h_out, w_out))
+    x_b = np.sum(colorized_img * q_b, 1).reshape((h_out, w_out))
 
-    X_a = cv.resize(X_a, (img_cols, img_rows), cv.INTER_CUBIC)
-    X_b = cv.resize(X_b, (img_cols, img_rows), cv.INTER_CUBIC)
+    x_a = cv.resize(x_a, (img_cols, img_rows), cv.INTER_CUBIC)
+    x_b = cv.resize(x_b, (img_cols, img_rows), cv.INTER_CUBIC)
 
-    X_a = X_a + 128
-    X_b = X_b + 128
+    x_a = x_a + 128
+    x_b = x_b + 128
 
     out_lab = np.zeros((img_rows, img_cols, 3), dtype=np.int32)
     out_lab[:, :, 0] = L
-    out_lab[:, :, 1] = X_a
-    out_lab[:, :, 2] = X_b
+    out_lab[:, :, 1] = x_a
+    out_lab[:, :, 2] = x_b
 
     out_lab = out_lab.astype(np.uint8)
     out_bgr = cv.cvtColor(out_lab, cv.COLOR_LAB2BGR)
